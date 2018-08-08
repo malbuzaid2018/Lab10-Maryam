dad2b36d117753675f11d2049daec682726201a9
malbuzaid2018 <malbuzaid2018@github.com>
2018-08-09 01:52:19 +0500

    Country Codes Class Updated

--- src/main/java/hw/CountryCodes.java
+++ src/main/java/hw/CountryCodes.java
@@ -2,10 +2,10 @@ package hw;
 
 public class CountryCodes {
 
-  static final String CN = "China";
-  static final String VN = "Vietnam";
-  static final String LA = "Laos";
   static final String MM = "Myanmar";
   static final String NP = "Nepal";
   static final String KG = "Kyrgyzstan";
+  static final String CN = "China";
+  static final String VN = "Vietnam";
+	
 }
